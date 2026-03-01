903695b30cf89ebbcd9edf5c5868bd5bae6679de
0smboy <osmboy@buaa.edu.cn>
2026-03-01 11:10:44 +0800

    chore: bump formula to v1.1.0

--- Formula/arliamp.rb
+++ Formula/arliamp.rb
@@ -1,8 +1,8 @@
 class Arliamp < Formula
   desc "Isolated cyber stage launcher for rliamp in Ghostty"
   homepage "https://github.com/0smboy/arliamp"
-  url "https://github.com/0smboy/arliamp/archive/refs/tags/v1.tar.gz"
-  sha256 "3ade813b4f5b70ff62fb09d25c360b9a0b0c453f59aaa2b8eae3f9a580072f13"
+  url "https://github.com/0smboy/arliamp/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "a9a76555458cd1491ff66a858883552f08c35e1e9b5c2eda5012eead612f593a"
   license :cannot_represent
 
   depends_on "rust" => :build
@@ -22,7 +22,7 @@ class Arliamp < Formula
   end
 
   test do
-    output = shell_output("#{bin}/arliamp 2>&1", 1)
-    assert_match "Usage: arliamp <music-directory>", output
+    output = shell_output("#{bin}/arliamp /definitely/not/found 2>&1", 1)
+    assert_match "arliamp: path not found", output
   end
 end
